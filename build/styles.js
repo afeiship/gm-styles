76fd05e3265a94cfb74e78dd6959719fdbaceb2b
afeiship <1290657123@qq.com>
2020-12-23 09:42:23 +0800

    feat: add flexbox

--- build/styles.js
+++ build/styles.js
@@ -1,18 +1,20 @@
-(function() {
+(function () {
   'use strict';
 
   const gulp = require('gulp');
-  const saveLicense = require('uglify-save-license');
+  const path = require('path');
+  var SASS_INCLUDE_PATHS = [path.join(__dirname, '..', '/node_modules/')];
   const $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del', '@feizheng/gulp-*', '@jswork/gulp-*']
   });
 
-  gulp.task('styles', function() {
+  gulp.task('styles', function () {
     return gulp
       .src('src/*.scss')
       .pipe($.jswork.pkgHeader())
-      .pipe($.sass({ outputStyle: 'compressed' }))
+      .pipe($.sass({ outputStyle: 'compressed', includePaths: SASS_INCLUDE_PATHS }))
       .pipe(gulp.dest('docs'))
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('dist'))
+      .pipe($.size({ title: '[ minimize size ]:' }));
   });
 })();
